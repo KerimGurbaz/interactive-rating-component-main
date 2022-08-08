1d194373f070d06408f467542ff95c15db1a583d
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-08 22:42:32 +0200

    updated

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let circles = document.querySelector(".circles");
 let submit = document.querySelector(".submit");
+let span = document.getElementsByTagName("span");
+let sec1 = document.querySelector(".sec1");
+let sec2 = document.querySelector(".sec2");
 
 
 circles.addEventListener("click",(e) => {
   if(e.target.classList.contains("one")){
     e.target.style.backgroundColor=`rgb(127,136,150)`;
     e.target.style.color="white";
-    console.log("one clicked");
+    sec1.style.visibility = "hidden";
+    
     
   }
   else if(e.target.classList.contains("two")){
@@ -32,10 +36,16 @@ circles.addEventListener("click",(e) => {
    else{
     console.log("other element clicked");
    }
-   submit.addEventListener("click",()=>{
-    document.getElementsByTagName("span").innerText = e.target.innerText
-    })
+ 
 })
 
 
 
+
+  
+submit.addEventListener("click",()=>{
+  sec1.style.visibility = "hidden";
+
+
+  span.innerText = e.target.innerText
+  })
