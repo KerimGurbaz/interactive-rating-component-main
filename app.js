049529c9e86502a91355fc5feb17b6b764d0845f
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-10 21:55:10 +0200

    done

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const sec1 = document.querySelector(".sec1");
-// const btns = document.querySelectorAll(".btn");
-// const content = document.querySelector(".content");
 const submit = document.querySelector(".submit");
 const sec2 = document.querySelector(".sec2");
-// const circles = document.querySelector(".circles");
 const span = document.querySelector("span");
 
 let selected ;
@@ -11,7 +8,7 @@ let selected ;
 
 sec1.addEventListener("click", (e) =>{
 selected =  e.target.value
-e.target.classList.contains("btn") ? e.target.style.color="white" &&  (e.target.style.backgroundColor = "rgb(127,135,151") && (e.target.style.color="white"): 
+e.target.classList.contains("btn") ? (e.target.style.color="white") &&  (e.target.style.backgroundColor = "rgb(127,135,151") : 
 
   console.log(selected);
 
