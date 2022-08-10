7f5f58b2925ea8fa32b9fbc276e684c4884f8fce
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-10 17:40:20 +0200

    updated

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const btns = document.querySelectorAll(".btn");
 const content = document.querySelector(".content");
 const submit = document.querySelector(".submit");
 const sec2 = document.querySelector(".sec2");
-const circles = document.querySelector(".circles");
+// const circles = document.querySelector(".circles");
+const span = document.querySelector("span");
 
 let selected ;
 
-ev.forEach((btn)=>{
-  btn.addEventListener("click",(event)=>{
-    console.log(event.target);
-selected = btn.value
-console.log(selected);
-  });
-});
 
+sec1.addEventListener("click", (e) =>{
+selected =  e.target.value
+e.target.style.backgroundColor ="grey";
+  console.log(selected);
+
+    submit.addEventListener("click", () =>{
+      sec2.style.display="block"
+      sec1.style.display = "none"
+      span.innerHTML = selected
+
+      console.log( `${selected} kullanildi`);
+    })
+})
