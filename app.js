4e806dca594f9d39d4a99e70e221e0163818adf4
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-10 21:24:45 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const sec1 = document.querySelector(".sec1");
-const btns = document.querySelectorAll(".btn");
-const content = document.querySelector(".content");
+// const btns = document.querySelectorAll(".btn");
+// const content = document.querySelector(".content");
 const submit = document.querySelector(".submit");
 const sec2 = document.querySelector(".sec2");
 // const circles = document.querySelector(".circles");
@@ -11,13 +11,14 @@ let selected ;
 
 sec1.addEventListener("click", (e) =>{
 selected =  e.target.value
-e.target.style.backgroundColor ="grey";
+e.target.classList.contains("btn") ? e.target.style.color="white" &&  (e.target.style.backgroundColor = "rgb(127,135,151") && (e.target.style.color="white"): 
+
   console.log(selected);
 
     submit.addEventListener("click", () =>{
-      sec2.style.display="block"
-      sec1.style.display = "none"
-      span.innerHTML = selected
+      sec2.style.display="block";
+      sec1.style.display = "none";
+      span.innerHTML = selected;
 
       console.log( `${selected} kullanildi`);
     })
