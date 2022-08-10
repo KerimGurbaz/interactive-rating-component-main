00be9a89e5cec3d20f0b6a6cf37a7bb9763a8eab
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-10 12:39:41 +0200

    updated

--- app.js
+++ app.js
@@ -1,51 +1,17 @@
-let circles = document.querySelector(".circles");
-let submit = document.querySelector(".submit");
-let span = document.getElementsByTagName("span");
-let sec1 = document.querySelector(".sec1");
-let sec2 = document.querySelector(".sec2");
+const sec1 = document.querySelector(".sec1");
+const btns = document.querySelectorAll(".btn");
+const content = document.querySelector(".content");
+const submit = document.querySelector(".submit");
+const sec2 = document.querySelector(".sec2");
+const circles = document.querySelector(".circles");
 
+let selected ;
 
-circles.addEventListener("click",(e) => {
-  if(e.target.classList.contains("one")){
-    e.target.style.backgroundColor=`rgb(127,136,150)`;
-    e.target.style.color="white";
-    sec1.style.visibility = "hidden";
-    
-    
-  }
-  else if(e.target.classList.contains("two")){
-    e.target.style.backgroundColor=`rgb(127,136,150)`;
-    e.target.style.color="white";
-    console.log("2 clicked");
-    }
-    else if(e.target.classList.contains("three")){
-        e.target.style.backgroundColor=`rgb(127,136,150)`;
-        e.target.style.color="white";
-        console.log("3 clicked");
-    }
-    else if(e.target.classList.contains("four")){
-        e.target.style.backgroundColor=`rgb(127,136,150)`;
-        e.target.style.color="white";
-        console.log("4 clicked");
-    }
-    else if(e.target.classList.contains("five")){
-        e.target.style.backgroundColor=`rgb(127,136,150)`;
-        e.target.style.color="white";
-        console.log("5 clicked");
-    }
-   else{
-    console.log("other element clicked");
-   }
- 
-})
+ev.forEach((btn)=>{
+  btn.addEventListener("click",(event)=>{
+    console.log(event.target);
+selected = btn.value
+console.log(selected);
+  });
+});
 
-
-
-
-  
-submit.addEventListener("click",()=>{
-  sec1.style.visibility = "hidden";
-
-
-  span.innerText = e.target.innerText
-  })
